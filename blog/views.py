bc59daf3ba799410c6ca370dd2c16cca737ba8f7
hedrya <hmaise2003@gmail.com>
2019-11-09 15:33:02 -0300

    seu joao

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Cadastro
 
 
 def post_list(request):
-    return render(request, 'post_list.html',{})
+    variavel = Cadastro.objects.all()
+    return render(request, 'post_list.html',{'variavel':variavel})
